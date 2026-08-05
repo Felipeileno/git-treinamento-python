926ef78b55d4f51b17de427da4c44a765eff5d31
Felipeileno <Felipe.ileno@vcpericia.com.br>
2026-08-05 16:38:50 -0400

    Adiciona função de multiplicação

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from calculadora import soma, subtracao
+from calculadora import soma, subtracao, multiplicacao
 print("=== Treinamento Git ===")
 a = 20
 b = 10
 print(f"Soma: {soma(a, b)}")
 print(f"Subtração: {subtracao(a, b)}")
+print(f"Multiplicação: {multiplicacao(a, b)}")
