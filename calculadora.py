7bf2549bf50ea609c5546eeeb93887a8ee65924e
Felipeileno <Felipe.ileno@vcpericia.com.br>
2026-08-06 08:14:15 -0400

    Adiciona função de divisão na calculadora

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,7 @@ def subtracao(a, b):
     return a - b
 def multiplicacao(a, b):
     return a * b
+def divisao(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Divisão por zero não permitida")
+    return a / b
